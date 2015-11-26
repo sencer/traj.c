cb5b0ca0ca64879588881f92a2769edb415714dc
Sencer Selcuk <sencerselcuk@gmail.com>
2015-11-25 21:17:28 -0500

    remove the old code

--- util.c
+++ util.c
@@ -15,11 +15,9 @@ int listDiff(int *l1, int len1, int *l2, int len2, int *ld, int *lend)
   // difference of l1 with len1 elements, and l2 with len2 elements
   // are written into ld with lend elements
   // enough memory for ld should be allocated in the caller
-  int flag;
   *lend = 0;
   for (int i = 0; i < len1; ++i)
   {
-    flag = 1;
     if (!isInList(l1[i], l2, len2)) ld[(*lend)++] = l1[i];
   }
   return 0;
