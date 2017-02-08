a64298f3f6a79d655eda734b1bd91a6b48cb1bf5
Sencer Selcuk <sencerselcuk@gmail.com>
2017-02-08 00:13:30 -0500

    order the atoms with respect to fragments they are in

---
+++ samples/fragment_order.c
@@ -0,0 +1,49 @@
+#include "xyz.h"
+#include "fragments.h"
+
+int checkBonding(double dist, int t1, int t2)
+{
+  int typ = t1 + t2;
+  return (dist<1.0||(typ>2 && dist<1.3)||(typ>10 && dist<2.0))?1:0;
+}
+
+
+int main(int argc, char *argv[])
+{
+  FILE *f = fopen(argv[1], "r");
+  int nat;
+  double dm[3];
+
+  // read the number of atoms and cell dimensions from lammpstrj
+  XYZReadHeader(f, &nat, dm);
+  // initialize a crystal structure with these dimensions
+  Crystal *c = CrystalInit(nat, dm);
+  // ...and a coarse graining box
+  CoarseBox *box = BoxInit(c, 2.0);
+  // ...and a bonding information container
+  BondingInfo *bnd = BondingInit(c);
+  // ...and a fragmentation information container
+  Fragments *frg = FragmentsInit(bnd);
+
+  XYZReadFrame(f, c);
+  BoxFill(c, box);
+  BondingPopulate(c, box, bnd, checkBonding);
+  FragmentsPopulate(bnd, frg);
+
+  int cur;
+  for (int i = 0; i < c->nat; ++i)
+  {
+    cur = frg->frags[i];
+    printf("%-2s %10.6f %10.6f %10.6f\n", PT_Symbol(c->atoms[cur].Z),
+      c->atoms[cur].coor[0], c->atoms[cur].coor[1], c->atoms[cur].coor[2]);
+  }
+
+  // free the memory
+  fclose(f);
+  FragmentsDelete(frg);
+  BondingDelete(bnd);
+  BoxDelete(box);
+  CrystalDelete(c);
+
+  return 0;
+}
